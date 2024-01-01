cb6ee051d7d77930ce4d82c0ef0cd1fe2890cd26
Vicente Jorquera <jorquerav07@gmail.com>
2023-12-31 21:47:29 -0300

    feat/se añaden modificaciones menores al layout de la versión móvil de la navbar

--- src/components/Navbar/MobileNav.tsx
+++ src/components/Navbar/MobileNav.tsx
@@ -14,7 +14,7 @@ const MobileNav = ({ open, setOpen }: { open: boolean, setOpen: any }) => {
           open ? { opacity: 1 } : { opacity: 0 }
         }
         transition={
-          open ? { duration: 0.5 } : { duration: 0}
+          open ? { duration: 0.5 } : { duration: 0 }
         }
         className={`${open ? 'h-full w-full md:hidden py-10 flex flex-col gap-2' : 'h-0 absolute pointer-events-none left-0 right-0 md:hidden opacity-0 pb-24'}`}
       >
@@ -44,27 +44,35 @@ const MobileNav = ({ open, setOpen }: { open: boolean, setOpen: any }) => {
           </Link>
         </motion.li>
 
-        <motion.ul className={
-          `flex-row gap-4 items-center fixed bottom-4 left-4 right-4 ${open ? 'flex h-fit' : 'h-0 pointer-events-none opacity-0'} md:hidden`
-        }>
-          {
-            SOCIAL_MEDIA && SOCIAL_MEDIA.map((item: any) => (
-              <motion.li
-                key={item.title}
-                whileHover={{ scaleY: 1.1 }}
-                whileFocus={{ scaleY: 1.1 }}
-                whileTap={{ scaleY: 0.9 }}
-                className='md:hidden border-b border-zinc-400 w-full pb-2 text-xl'
-                onClick={() => setOpen(!open)}
-              >
-                <Link
-                href={item.href} className='text-stone-500 font-normal' >
-                  {item.icon ? item.icon : item.title}
-                </Link>
-              </motion.li>
-            ))
-          }
-        </motion.ul>
+        <motion.section
+        className={`flex-col gap-8 items-start fixed bottom-4 left-4 right-4 ${open ? 'flex h-fit' : 'h-0 pointer-events-none opacity-0'} md:hidden`}>
+          <motion.h2
+            className='text-xl font-medium text-stone-500'
+          >
+            Sígueme en mis redes sociales
+          </motion.h2>
+          <motion.ul className={
+            `w-full flex flex-row gap-4 justify-center items-center`
+          }>
+            {
+              SOCIAL_MEDIA && SOCIAL_MEDIA.map((item: any) => (
+                <motion.li
+                  key={item.title}
+                  whileHover={{ scaleY: 1.1 }}
+                  whileFocus={{ scaleY: 1.1 }}
+                  whileTap={{ scaleY: 0.9 }}
+                  className='md:hidden border-b border-zinc-400 w-full pb-2 text-xl text-center'
+                  onClick={() => setOpen(!open)}
+                >
+                  <Link
+                    href={item.href} className={`text-stone-500 font-normal hover:text-[${item.highlight_color}]`} >
+                    {item.icon ? item.icon : item.title}
+                  </Link>
+                </motion.li>
+              ))
+            }
+          </motion.ul>
+        </motion.section >
       </motion.ul>
     </>
   )
